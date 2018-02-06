e7ecdcc6014ad09d8ba68c16978310c4ded66236
Daniel Porto <daniel.porto@gmail.com>
2018-02-06 13:24:16 +0000

    updates on envs

--- envs.sh
+++ envs.sh
@@ -8,7 +8,6 @@ export PATH=$PATH:$GOPATH/bin
 # export PATH="$HOME/.jenv/bin:$PATH"
 # eval "$(jenv init -)"
 
-export HOMEBREW_GITHUB_API_TOKEN="fd700f1f835dc28f3b41eb9f90db3ad40ae4616f"
 # read other envs with keys and stuff
 if [ -n $HOME/.ssh/sensible-envs.sh ]; then 
     source $HOME/.ssh/sensible-envs.sh
